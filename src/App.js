7c383fc89e3edbd3ee9ce07b5178a56a31184dba
Bob Whitelock <bob.whitelock1@gmail.com>
2019-06-28 18:10:24 -0700

    Get minimal graph drawing working :tada:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,94 @@
 import React, {Component} from 'react';
 import './App.css';
 import _ from 'lodash';
+const d3 = require('d3');
 
-class LambdaDemo extends Component {
+const WIDTH = 1600;
+const HEIGHT = 1000;
+const GRAPH_ROOT_ID = 'graph-root';
+
+// All force-directed graph stuff copied and adapted from
+// https://observablehq.com/@d3/force-directed-graph?collection=@d3/d3-force.
+
+const drag = simulation => {
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  return d3
+    .drag()
+    .on('start', dragstarted)
+    .on('drag', dragged)
+    .on('end', dragended);
+};
+
+function drawD3Graph(data) {
+  const svg = d3.select(`#${GRAPH_ROOT_ID}`);
+  if (!svg) {
+    return;
+  }
+
+  const links = data.links.map(d => Object.create(d));
+  const nodes = data.nodes.map(d => Object.create(d));
+
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force('link', d3.forceLink(links).id(d => d.id))
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(WIDTH / 2, HEIGHT / 2));
+
+  svg.attr('viewBox', [0, 0, WIDTH, HEIGHT]);
+
+  const link = svg
+    .append('g')
+    .attr('stroke', '#999')
+    .attr('stroke-opacity', 0.6)
+    .selectAll('line')
+    .data(links)
+    .join('line')
+    .attr('stroke-width', d => Math.sqrt(d.value));
+
+  const node = svg
+    .append('g')
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 1.5)
+    .selectAll('circle')
+    .data(nodes)
+    .join('circle')
+    .attr('r', 5)
+    // XXX colours
+    .attr('fill', 'gray')
+    .call(drag(simulation));
+
+  node.append('title').text(d => d.name);
+
+  simulation.on('tick', () => {
+    link
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y);
+
+    node.attr('cx', d => d.x).attr('cy', d => d.y);
+  });
+
+  // invalidation.then(() => simulation.stop());
+}
+
+class DrawGraph extends Component {
   constructor(props) {
     super(props);
     this.state = {users: {}, tacos: {}};
@@ -85,7 +171,10 @@ class LambdaDemo extends Component {
     console.log('tacos:', tacos);
     console.log('nodes:', nodes);
     console.log('links:', links);
-    return <p />;
+
+    drawD3Graph({nodes, links});
+
+    return null;
   }
 }
 
@@ -94,7 +183,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <LambdaDemo />
+          <svg id={GRAPH_ROOT_ID} width={WIDTH} height={HEIGHT} />
+          <DrawGraph />
         </header>
       </div>
     );
