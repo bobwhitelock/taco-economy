60055d0f4ea40a73b17fb94839677b0d71fe274e
Bob Whitelock <bob.whitelock1@gmail.com>
2019-07-01 11:58:36 -0700

    WIP debugging

--- src/lambda/tacos.js
+++ src/lambda/tacos.js
@@ -3,10 +3,16 @@ import MongoClient from 'mongodb';
 const mongodb_uri = process.env.MONGODB_URI;
 
 export function handler(event, context, callback) {
+  // eslint-disable-next-line no-console
+  console.log('context [ucasneys]:', context);
+  // eslint-disable-next-line no-console
+  console.log('event [idvamozh]:', event);
   return MongoClient.connect(mongodb_uri, function(err, client) {
     // eslint-disable-next-line no-console
-    console.log('mongodb_uri [gklzwgxp]:', mongodb_uri);
+    console.error('mongodb_uri [gklzwgxp]:', mongodb_uri);
     if (err) {
+      // eslint-disable-next-line no-console
+      console.error('err [byehqpwu]:', err);
       throw err;
     }
 
@@ -17,7 +23,7 @@ export function handler(event, context, callback) {
       .toArray()
       .then(tacos => {
         // eslint-disable-next-line no-console
-        console.log('tacos [kesdsrvh]:', tacos);
+        console.error('tacos [kesdsrvh]:', tacos);
         const error = null;
         const response = {
           statusCode: 200,
